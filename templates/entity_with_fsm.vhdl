8f679f9c0893e29d9591e504579799ff904162bf
its-fonsy <ciabadiale@gmail.com>
2025-02-13 16:58:46 +0100

    Refactoring templates * changing TAB from 2 to 4 spaces to make the code more readable; * adding the label at the end of entities/processes/architectures;

--- templates/entity_with_fsm.vhdl
+++ templates/entity_with_fsm.vhdl
@@ -3,52 +3,52 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ENTITY_NAME is
-  port (
-        clk   : in  std_logic;
-        rstn  : in  std_logic
-       );
-end ENTITY_NAME;
+    port (
+             clk   : in  std_logic;
+             rstn  : in  std_logic
+         );
+end entity ENTITY_NAME;
 
 architecture RTL of ENTITY_NAME is
 
-  -- FSM states type definition
-  type fsm_state is (FSM_INIT);
+    -- FSM states type definition
+    type fsm_state is (FSM_INIT);
 
-  -- Constants definition
+    -- Constants definition
 
-  -- Signals
-  signal cs, ns : fsm_state;
+    -- Signals
+    signal cs, ns : fsm_state;
 
 begin
 
-  -- FSM state register
-  fsm_reg: process(clk) is
-  begin
-    if rising_edge(clk) then
-      if rstn = '0' then
-        cs <= FSM_INIT;
-      else
-        cs <= ns;
-      end if;
-    end if;
-  end process;
+    -- FSM state register
+    fsm_reg: process(clk) is
+    begin
+        if rising_edge(clk) then
+            if rstn = '0' then
+                cs <= FSM_INIT;
+            else
+                cs <= ns;
+            end if;
+        end if;
+    end process fsm_reg;
 
-  -- FSM datapath
-  fsm_datapath: process(cs) is
-  begin
+    -- FSM datapath
+    fsm_datapath: process(cs) is
+    begin
 
-    -- Default signals values
+        -- Default signals values
 
-    -- State switch case
-    case cs is
+        -- State switch case
+        case cs is
 
-      when FSM_INIT =>
-        ns <= FSM_INIT;
+            when FSM_INIT =>
+                ns <= FSM_INIT;
 
-      when others =>
-        ns <= FSM_INIT;
+            when others =>
+                ns <= FSM_INIT;
 
-    end case;
-  end process;
+        end case;
+    end process fsm_datapath;
 
-end RTL;
+end architecture RTL;
--- templates/testbench.vhdl
+++ templates/testbench.vhdl
@@ -6,48 +6,48 @@ use std.env.finish;
 
 -- Empty entity for testbench
 entity TESTBENCH is
-end TESTBENCH;
+    end TESTBENCH;
 
 architecture behav of TESTBENCH is
 
-  -- Constants definition
-  constant clock_frequency  : integer := 100e6;
-  constant clock_period     : time := 1000 ms / clock_frequency;
+    -- Constants definition
+    constant clock_frequency  : integer := 100e6;
+    constant clock_period     : time := 1000 ms / clock_frequency;
 
-  -- Component definition
-  component ENTITY_TO_TEST is
-    port(
-        clk : in std_logic
-        );
-  end component;
+    -- Component definition
+    component ENTITY_TO_TEST is
+        port(
+                clk : in std_logic
+            );
+    end component;
 
-  -- Signals definitions
-  signal clk         : std_logic := '0';
-  signal rstn        : std_logic;
+    -- Signals definitions
+    signal clk         : std_logic := '0';
+    signal rstn        : std_logic;
 
 begin
 
-  dut: ENTITY_TO_TEST port map(clk);
+    dut: ENTITY_TO_TEST port map(clk);
 
-  -- Clock process
-  clk <= not clk after clock_period / 2;
+    -- Clock process
+    clk <= not clk after clock_period / 2;
 
-  -- Main simulation process
-  stimuli: process is
-  begin
+    -- Main simulation process
+    stimuli: process is
+    begin
 
-    -- Initialize the signals
-    rstn <= '1';
-    wait until falling_edge(clk);
-    
-    -- Reset the system
-    rstn <= '0';
-    wait until falling_edge(clk);
-    rstn <= '1';
+        -- Initialize the signals
+        rstn <= '1';
+        wait until falling_edge(clk);
 
-    report "Test finished";
-    finish;
+        -- Reset the system
+        rstn <= '0';
+        wait until falling_edge(clk);
+        rstn <= '1';
 
-  end process;
+        report "Test finished";
+        finish;
+
+    end process;
 
 end behav;
